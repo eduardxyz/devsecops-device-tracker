854fe054f8b47bfdef1574ed8e22ab6e86003ee6
Eduard Marian <55080847+eduardxyz@users.noreply.github.com>
2026-02-18 18:37:11 +0200

    feat(device-registration): add requirements and database module

---
+++ device_registration_api/app/database.py
@@ -0,0 +1,14 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
